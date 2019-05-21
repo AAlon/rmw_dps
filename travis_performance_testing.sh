98558a6617f2f7ecef09590f639fd3697852fe73
Avishay Alon <avishaya@amazon.com>
2019-05-21 23:51:38 +0000

    Sourcing dashing prior to sourcing the local ws
    Signed-off-by: Avishay Alon <avishaya@amazon.com>

--- travis_performance_testing.sh
+++ travis_performance_testing.sh
@@ -3,4 +3,4 @@ set -e
 
 git clone https://github.com/irobot-ros/ros2-performance ${TRAVIS_BUILD_DIR}/../ros2-performance
 docker exec osrf_ros2_nightly /bin/bash -c "apt-get update && source /opt/ros/dashing/setup.bash && rosdep update && rosdep install --from-paths /shared/ros2-performance --ignore-src -r -y"
-docker exec osrf_ros2_nightly /bin/bash -c "source /shared/rmw_dps/install/local_setup.bash && export RMW_IMPLEMENTATION=rmw_dps_cpp && colcon build --packages-up-to benchmark"
+docker exec osrf_ros2_nightly /bin/bash -c "source /opt/ros/dashing/setup.bash && source /shared/rmw_dps/install/local_setup.bash && export RMW_IMPLEMENTATION=rmw_dps_cpp && colcon build --packages-up-to benchmark"
